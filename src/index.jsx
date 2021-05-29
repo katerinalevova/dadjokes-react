6f94ce53144c7484a497d8e1ba9de61e50e32488
Kateřina Levová <katerina.levova@gmail.com>
2021-05-29 16:30:18 +0200

    Naimportovan jokes.js a zobrazeny všechny vtipy

--- src/index.jsx
+++ src/index.jsx
@@ -2,41 +2,9 @@ import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
 import Joke from './Joke/index.jsx';
+import { jokes } from './jokes';
 
 const App = () => {
-  const jokes = [
-    {
-      id: 'user01',
-      avatar:
-        'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png',
-      name: 'Neroxx',
-      text: 'The secret service isn\'t allowed to yell "Get down!" anymore when the president is about to be attacked. Now they have to yell "Donald, duck!"',
-      likes: 13,
-      dislikes: 24,
-    },
-    {
-      id: 'user02',
-      avatar:
-        'https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user02.png',
-      name: 'wetcxjfsj',
-      text: "Why did the invisible man turn down the job offer? He couldn't see himself doing it.",
-      likes: 50,
-      dislikes: 10,
-    },
-  ];
-  // const ZobrazeniVtipu = (jokes) => {
-  //   for (let i = 0; i < length.jokes; i++) {
-  //     <Joke
-  //       userAvatar={jokes[i].avatar}
-  //       userName={jokes[i].name}
-  //       text={jokes[i].text}
-  //       likes={jokes[i].likes}
-  //       dislikes={jokes[i].dislikes}
-  //     />;
-  //   }
-  //   return <Joke />;
-  // };
-
   return (
     <div>
       {jokes.map((vtip) => {
@@ -56,16 +24,3 @@ const App = () => {
 };
 
 render(<App />, document.querySelector('#app'));
-
-// const App = () => (
-//   <div>
-//     {' '}
-//     {cities.map((mesto) => {
-//       return (
-//         <div className="city" key={mesto}>
-//           {mesto}
-//         </div>
-//       );
-//     })}{' '}
-//   </div>
-// );
